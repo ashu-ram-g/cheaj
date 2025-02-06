6a7d5f8aea46a6d2cd9fc6a1f99eec2c768bba05
ashu-ram-g <jairam40101@gmail.com>
2025-02-06 14:22:28 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 # config.py
 BOT_NAME = "Free bot"
-BOT_TOKEN = "6755775439:AAGkahjp3xK71u-jG6V0uQUR-xJgqLPt9yw"
-API_ID = 28328736
-API_HASH = "802254a44896baa87f3083b7af36b2e5"
+BOT_TOKEN = "7958229600:AAHqpzrkOdzCNn-0QF5JOsi6bLvuxn9ETBM"
+API_ID = 18116881
+API_HASH = "cca3bacf40fb3ebcb4f075b2e46ff1bd"
 MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
